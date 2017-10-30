114773d3533959709c4fe7d86820b3f7595af529
claudialm <claudia_incondicional@hotmail.com>
2017-10-30 20:32:25 -0300

    Esta clase recibe datos  por ventana

---
+++ src/GuardarDatosPersonales.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB03
+ */
+public class GuardarDatosPersonales 
+{
+  public static void main(String[] args)
+  {    
+     String nombre=("Digite su nombre");
+     String edad=("Digite su edad");
+      
+     System.out.println("Bienvenido "+nombre");
+     
+  }        
+    
+
+
+
+
+}
